71678a06f42aadd5e63edf553b4b4b2128687d0c
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-01-11 03:27:13 +0100

    Added static assets, installed ejs package and rendered html from the express server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,26 @@
 import express from "express"
 
-console.log(express)
+// console.log(express)
+
+const PORT = 4000
+const HOST = "0.0.0.0"
+const htmlGreeting =
+  "Dear <strong>User</strong> , we are pleased  to have you onboard"
+
+const server = express()
+
+server.use(express.static("dist"))
+
+server.set("view engine", "ejs")
+
+server.use("/", (req, res) => {
+  res.render("index", {
+    variable:
+      "This is the value of an ejs variable passed from express",
+    greeting: htmlGreeting
+  })
+})
+
+server.listen(PORT, HOST, () => {
+  console.info(`Server listening on port ${PORT} in ${HOST}`)
+})
